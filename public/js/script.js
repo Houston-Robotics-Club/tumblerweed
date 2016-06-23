90ed0a3a4d0649e0b0c2cd3bbedbc8779da5166c
dtex <donovan@donovan.bz>
2016-06-23 09:52:47 -0500

    Add commands for more natural differential drive control

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,25 @@
 var stick = new VirtualJoystick({
-  mouseSupport: true,
-  container: document.getElementById("stick"),
+  //mouseSupport: true,
+  container: document.getElementById("stickContainer"),
   strokeStyle: 'blue',
   limitStickTravel: true
 });
 
-// stick.addEventListener('touchStartValidation', function(event){
-//   var touch	= event.changedTouches[0];
-//   console.log(touch.pageX, touch.pageY);
-//   if( touch.pageX >= window.innerWidth/2 &&  touch.pageY > window.innerHeight/2)	return true;
-//   return false;
-// });
+stick.addEventListener('touchStartValidation', function(event){
+  var touch	= event.changedTouches[0];
+  //console.log(touch.pageX, touch.pageY);
+  //if( touch.pageX >= window.innerWidth/2 &&  touch.pageY > window.innerHeight/2)	return true;
+  return true;
+});
+
+var greater = function(a, b) {
+  return a > b ? a : b;
+};
+
+
+var lesser = function(a, b) {
+  return a > b ? b : a;
+};
 
 var listener = new window.keypress.Listener();
 
@@ -23,7 +32,50 @@ var commands = [
   {"com": "right", "key": "right" },
   {"com": "reverse", "key": "down" },
   {"com": "stop", "key": "space" },
+  {"com": "drive", "key": "d" },
 ];
+
+var lastLeft, lastRight;
+
+setInterval(function(){
+  var left, right;
+  var outputEl	= document.getElementById('result');
+
+  var rawForward = Number(stick.deltaY());
+  var rawAlt = Number(stick.deltaX());
+
+  if (rawForward === 0 && rawAlt === 0) {
+    socket.emit("stop");
+    return;
+  }
+
+  var maxDiff = rawForward < 0 ? (-100 - rawForward) : (100 - rawForward);
+
+  var diff = rawAlt >= 0 ? lesser(maxDiff, rawAlt): greater(maxDiff, rawAlt);
+
+  if ((rawForward < 0 && rawAlt > 0) || (rawForward > 0 && rawAlt < 0)) {
+    left = rawForward + diff;
+    right = rawForward - diff;
+  }
+  if ((rawForward < 0 && rawAlt < 0) || (rawForward > 0 && rawAlt > 0)) {
+    left = rawForward - diff;
+    right = rawForward + diff;
+  }
+
+  if (left != lastLeft || right != lastRight) {
+    if (left === 0 && right === 0) {
+      socket.emit("stop");
+    } else {
+      socket.emit("drive", {left: left, right: right});
+    }
+
+    lastLeft = left;
+    lastRight = right;
+  }
+
+  outputEl.innerHTML	= "left: " + left + " right: " + right;
+
+}, 1/10 * 1000);
 
 // Loop through our commands
 commands.forEach( function(command) {
